494326a847d862ddeab59ca36c3a0083bd2ba50e
Jose Carranco <josecarranco@joses-mbp.attlocal.net>
2020-05-14 00:09:11 -0500

    Reject a Promise and Handle Errors

--- js/promises-breakfast.js
+++ js/promises-breakfast.js
@@ -1,9 +1,17 @@
+const order = false;
 
 const breakfastPromise = new Promise( (resolve, reject) => {
- setTimeout( ()=>{
-    resolve('Your order is ready, come and get it.');
+ setTimeout(()=>{
+    if(order){
+        resolve('Your order is ready, come and get it');
+    } else {
+        reject( Error('Your order can not be made.') );
+    }
  }, 3000)
 });
 
 console.log(breakfastPromise);
-breakfastPromise.then(val => console.log(val));
+breakfastPromise
+    .then(val => console.log(val))
+    .catch(err => console.log(err)
+)
